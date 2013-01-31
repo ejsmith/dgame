3401cc50d5438b5625caf2b5a47be3fcfb5e63c2
Eric J. Smith <eric@codesmithtools.com>
2013-01-31 15:10:13 -0600

    Removed packages director and added a gitignore file.

--- packages/dgame/src/game_entity_renderer.dart
+++
@@ -1,7 +0,0 @@
-part of dgame;
-
-class GameEntityRenderer<E extends GameEntity> {
-  GameRenderer gr;
-  GameEntityRenderer(GameRenderer this.gr);
-  void render(E e) {}
-}
--- packages/dgame/src/game_loop.dart
+++
@@ -1,9 +0,0 @@
-part of dgame;
-
-typedef void LoopCallback();
-
-class GameLoop {
-  start(LoopCallback callback) {
-    new Timer.repeating(50, (t) => callback());
-  }
-}
