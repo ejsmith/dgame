efb7acf505678e6e0dd089dcc5e43a7ff074b7bd
Cody Smith <cody@ericjsmith.net>
2013-08-15 16:00:27 -0500

    Trying to fix audio

--- lib/src/game_sound.dart
+++ lib/src/game_sound.dart
@@ -6,21 +6,21 @@ class GameSound {
   String Sound;
   double Volume;
   bool Looping;
-  String _rootDirectory = "Sounds/";
+  String _rootDirectory = "sounds/";
   String get rootDirectory => _rootDirectory;
   set rootDirectory(String value) {
     if (!value.endsWith("/"))
       value = value + "/";
     _rootDirectory = value;
   }
-  
+
   GameSound(String sound, [double volume = 1.0, bool looping = false]) {
     Sound = sound;
     Volume = volume;
     Looping = looping;
   }
-  
+
   void play(String sound, [double volume = 1.0, bool looping = false]) {}
-  
+
   void remove() {}
 }
--- lib/src/html/html_game_sound.dart
+++ lib/src/html/html_game_sound.dart
@@ -2,62 +2,57 @@ part of dgame_html;
 
 class HtmlGameSound extends GameSound {
   HtmlGameSound(String sound, [double volume = 1.0, bool looping = false]) : super(sound, volume, looping) {}
-  
+
   AudioElement audioCopy;
-  
+
   void play(String sound, [double volume = 1.0, bool looping = false]) {
     if (!enabled)
       return;
-    
+
     if (Game.debugMode)
       print("Playing: $sound");
-    
+
     AudioElement sourceAudio = query("#sound_$sound");
     if (sourceAudio == null)
       sourceAudio = query("#$sound");
-    
+
     if (sourceAudio == null) {
       sourceAudio = new AudioElement();
       sourceAudio.id = "sound_$sound";
       sourceAudio.preload = "auto";
-      
+
       var s = new SourceElement();
-      s.src = "$rootDirectory$sound.mp3";
-      s.type = "audio/mp3";
-      sourceAudio.nodes.add(s);
-      
-      s = new SourceElement();
       s.src = "$rootDirectory$sound.ogg";
       s.type = "audio/ogg";
       sourceAudio.nodes.add(s);
-      
+
       document.body.nodes.add(sourceAudio);
     }
-    
+
     audioCopy = sourceAudio.clone(true) as AudioElement;
     audioCopy.id = "";
     document.body.nodes.add(audioCopy);
-    
+
     audioCopy.onEnded.listen((e) {
       if (Game.debugMode)
         print("removed");
       audioCopy.remove();
     });
-    
-    if (!enabled) 
+
+    if (!enabled)
       audioCopy.remove();
-    
+
     if (volume != null)
       audioCopy.volume = round(volume, 3);
-    
+
     if (looping)
       audioCopy.loop = true;
 
-    
+
     audioCopy.play();
     audioCopy.classes.add("sound-clone");
   }
-  
+
   void remove() {
     audioCopy.currentTime = 999;
     audioCopy.pause();
